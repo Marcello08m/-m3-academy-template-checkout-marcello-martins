2dab582c699f6cccee212ae8865ecad09266a6c0
Caroline Moran <carol.moran@agenciam3.com>
2022-12-08 11:01:04 -0400

    feat(Footer.js): add função que monitora se o carrinho está vazio

--- checkout/src/arquivos/js/components/Footer.js
+++ checkout/src/arquivos/js/components/Footer.js
@@ -7,15 +7,28 @@ export default class Footer {
 
     async init() {
         await this.selectors();
-        console.log(this.item);
+        // this.onUpdate();
     }
 
     async selectors() {
-        this.item = await waitElement("#my-element", {
-            //#my-element pode ser a class ou o id do elemento html qeu vocÊ quer pegar
-            timeout: 5000, // vai esperar 5 segundos antes de rejeitar a promise
-            interval: 1000, // vai verificar a cada 1 segundo se o elemento existe
+        //Para verificar se o carrinho está vazio e remover a prateleira de produtos:
+        // vocês devem olhar a doc fornecida no Desafio para aprender a usar o waitElement
+        this.checkoutVazio = await waitElement(".empty-cart-content");
+    }
+
+    onUpdate() {
+        //Função qeu fará a verificação se o carrinho está vazio para remover a prateleira de produtos:
+        // vocês devem olhar a doc fornecida no Desafio para aprender a usar a MutationObserver
+        // sempre que o carrinho estiver vazio o elemento chcekoutVazio fica display: none e isso pode ser usado como atributo para a MutationObserver
+        let target = this.checkoutVazio;
+        let config = { childList: true, attributes: true };
+        let observer = new MutationObserver((mutations) => {
+            mutations.forEach(function (mutation) {
+                console.log(mutation.type);
+            });
         });
+
+        observer.observe(target, config);
     }
     async addCarrossel() {
         const elemento = await waitElement("#my-element");
